f98346761ef5db7a498cbea0fa8427b56e62d1ce
Ramiro <vip@ramiroamorim.com.br>
2025-12-15 10:12:59 -0300

    gtm e body

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,15 @@ export default function RootLayout({
           // Init será feito pelo FacebookPixelInit com dados hasheados`}
         </Script>
 
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TV7262BX');`}
+        </Script>
+
 
 
         <noscript>
@@ -59,11 +68,21 @@ export default function RootLayout({
 
 
    <body className={cn("antialiased bg-black font-adero")}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TV7262BX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
          <AmplitudeInit />
           <TrackingInit />
           <FacebookPixelInit />
-       
+
 
           {children}
         </ThemeProvider>
